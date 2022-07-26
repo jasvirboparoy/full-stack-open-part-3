2ad17e26e59b9bd4fb8d19a025414c6c9adcfb3d
Jasvir Boparoy <j.boparoy@student.unsw.edu.au>
2022-07-26 21:35:32 +1000

    Add files for phonebook backend

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const e = require('express')
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -121,7 +124,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
